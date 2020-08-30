0251369d8d51c0d9364a660981bc037d85e5c415
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-08-30 10:41:15 -0300

    Created proof of work function.

--- module_1_create_blockchain/blockchain.py
+++ module_1_create_blockchain/blockchain.py
@@ -1,5 +1,5 @@
 import datetime
-import hashlab
+import hashlib
 import json
 from flask import Flask, jsonify
 
@@ -21,3 +21,15 @@ class Blockchain:
     
     def get_previous_block(self):
         return self.chain[-1] # get the last element of list
+    
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
