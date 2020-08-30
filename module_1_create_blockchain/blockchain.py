ed44c395a5e1ddda20fe90b5508cb01ad2725a8d
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-08-30 15:17:54 -0300

    Created function that mine block in the blockchain

--- module_1_create_blockchain/blockchain.py
+++ module_1_create_blockchain/blockchain.py
@@ -57,3 +57,17 @@ class Blockchain:
     
 app = Flask(__name__)
 blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['previous_proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Parabens! Voce minerou um bloco!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
